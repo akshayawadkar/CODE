d809a299a1896c99c4d32de5733ebec1ebbffe88
akshayanilwadkar <mailaw3515@gmail.com>
2021-11-30 19:52:37 -0800

    adding new

--- gtci/sliding.window/LongestSubstringKDistinct.java
+++ gtci/sliding.window/LongestSubstringKDistinct.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestSubstringKDistinct {
 
     public static void main(String[] args) {
@@ -14,6 +17,28 @@ public class LongestSubstringKDistinct {
 
         int left = 0, right = 0;
         int n = s.length();
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        while (right < n) {
+            char ch = s.charAt(right);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+
+            if (map.size() <= k) {
+                result = Math.max(result, right - left + 1);
+            } else {
+                while (map.size() >= k) {
+                    ch = s.charAt(left);
+                    map.put(ch, map.getOrDefault(ch, 0) - 1);
+                    if (map.get(ch) <= 0) {
+                        map.remove(ch);
+                    }
+                    left++;
+                }
+            }
+
+            right++;
+        }
 
         return result;
     }
